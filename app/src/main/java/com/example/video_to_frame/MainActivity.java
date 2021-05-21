823ba5570c16efa6a804ef112daaf7675e38225c
cayl-crypto <caylio64@gmail.com>
2021-05-21 14:00:24 +0300

    TODO added.

--- app/src/main/java/com/example/video_to_frame/MainActivity.java
+++ app/src/main/java/com/example/video_to_frame/MainActivity.java
@@ -11,4 +11,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //TODO: ADD UI items. Camera button, TextView, cameraview.
+    //TODO: ADD background operations to the camera button.
+    //TODO: Take 5 seconds video when camera opened, extract 8 frames from the video and assign frames to variables.
+
 }
