2aea23e948661cc27b1f79900bf92ce8bff0aede
selman-aydin <selman19071999@gmail.com>
2021-05-21 16:51:23 +0300

    second commit

--- app/src/main/java/com/example/video_to_frame/MainActivity.java
+++ app/src/main/java/com/example/video_to_frame/MainActivity.java
@@ -1,14 +1,68 @@
 package com.example.video_to_frame;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    Button camerabtn;
+    private VideoView videoView;
+    static final int REQUEST_VIDEO_CAPTURE = 1;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        camerabtn = findViewById(R.id.cameraButton);
+        videoView = findViewById(R.id.video_view);
+
+        camerabtn.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View v) {
+                cameraStart(videoView);
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void cameraStart(View view) {
+//        ContentValues values = new ContentValues();
+//        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+//        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
+//        mImageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+//        //        Camera Intent
+//        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
+//        startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+//
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT,5);
+        startActivityForResult(intent,1);
+        onActivityResult(1,1,intent);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            Uri videoUri = intent.getData();
+            videoView.setVideoURI(videoUri);
+        }
     }
 }
+
